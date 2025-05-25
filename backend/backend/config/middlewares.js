dc5d314f6b3839d3d8981bf28813a9ff28e68514
Intranik <intranik.khazro@hotmail.com>
2025-05-25 23:50:08 +0200

    middlewares

--- backend/backend/config/middlewares.js
+++ backend/backend/config/middlewares.js
@@ -1,9 +1,27 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://127.0.0.1:5500'], // eller där din HTML körs
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
